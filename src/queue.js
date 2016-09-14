276c0185c73fe0333a111447dab5f3a3a389c7af
sasha-klimashevich <aleksandra.klimashevich@gmail.com>
2016-09-14 14:56:50 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,25 +1,39 @@
 const MaxHeap = require('./max-heap.js');
 
-class PriorityQueue extends MaxHeap {
-	constructor(maxSize) {
+class PriorityQueue {
+    constructor(maxSize) {
+        if (maxSize) {
+            this.maxSize = maxSize;
+        } else {
+            this.maxSize = 30;
+        }
+        this.heap = new MaxHeap();
+    }
 
-	}
+    push(data, priority) {
+        if (this.size() == this.maxSize) {
+            throw new Error("Error: queue has max size");
+        }
+        this.heap.push(data, priority);
+    }
 
-	push(data, priority) {
+    shift() {
+        if (this.heap.isEmpty()) {
+            throw new Error("Error: queue is empty");
+        } else {
+            let valueOfRemovedNode;
+            valueOfRemovedNode = this.heap.pop();
+            return valueOfRemovedNode;
+        }
+    }
 
-	}
+    size() {
+        return this.heap.size();
+    }
 
-	shift() {
-
-	}
-
-	size() {
-
-	}
-
-	isEmpty() {
-		return true;
-	}
+    isEmpty() {
+        return this.heap.isEmpty();
+    }
 }
 
 module.exports = PriorityQueue;
